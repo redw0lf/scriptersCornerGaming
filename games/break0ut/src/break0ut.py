4c29aec18ab2baefacbbe864eb253bca2f17c8bc
redw0lf <redw0lf@dev>
2013-04-04 21:56:21 +0000

    Converted single file to a multi file system Added Block class and according image Added GameElement as a base class for all other classes

--- games/break0ut/src/break0ut.py
+++ games/break0ut/src/break0ut.py
@@ -3,107 +3,22 @@ Created on Mar 25, 2013
 
 @author: redw0lf
 '''
-import pygame, sys,os,random
+import pygame, sys, os, random
 from pygame.locals import * 
-
-#functions to create our resources
-def load_image(name, colorkey=None):
-    fullname = os.path.join('../data', name)
-    try:
-        image = pygame.image.load(fullname)
-    except pygame.error, message:
-        print 'Cannot load image:', fullname
-        raise SystemExit, message
-    image = image.convert()
-    if colorkey is not None:
-        if colorkey is -1:
-            colorkey = image.get_at((0,0))
-        image.set_colorkey(colorkey, RLEACCEL)
-    return image, image.get_rect()
+from breakout.data.Ball import Ball
+from breakout.data.Bar import Bar
+from breakout.data.Block import Block
 
 
-#classes for our game objects
-class Bar(pygame.sprite.Sprite):
-    """moves a clenched fist on the screen, following the mouse"""
-    def __init__(self, yPos):
-        pygame.sprite.Sprite.__init__(self) #call Sprite initializer
-        self.image, self.rect = load_image('fist.bmp', -1)
-        self.computerControlled = False
-        screen = pygame.display.get_surface()
-        self.area = screen.get_rect()
-        self.move = 5
-        self.image=pygame.transform.rotate(self.image,90)
-        self.rect = self.image.get_rect()
-        
-        self.yPos = yPos
-
-    def update(self):
-        "move the fist based on the mouse position"
-        pos = pygame.mouse.get_pos()
-        newpos = self.rect.move((self.move,0))
-        newpos.centery = self.yPos
-        if not self.computerControlled:
-            self.rect.center = (pos[0],self.yPos)
-        else:
-            self.rect.center
-            if self.rect.left < self.area.left or \
-               self.rect.right > self.area.right:
-                self.move = -self.move
-                newpos = self.rect.move((self.move,0))
-                
-            self.rect = newpos
-    def increaseScore(self):
-        """increases the score of the desired bar"""
-        self.score = self.score+1
-
-
-class Ball(pygame.sprite.Sprite):
-    """moves a clenched fist on the screen, following the mouse"""
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self) #call Sprite initializer
-        self.image, self.rect = load_image('nicubunu_Monkey_head.png', -1)
-        self.image = pygame.transform.scale(self.image,(25,25))
-        self.rect.w =25
-        self.rect.h =25
-        self.slope = 1
-        screen = pygame.display.get_surface()
-        self.area = screen.get_rect()
-        self.move = -2
-        self.rect.center = self.area.center
-
-    def update(self):
-        "move the fist based on the mouse position"
-        
-        
-
-        "lets the ball reflect from the walls"
-        if self.rect.top < self.area.top or \
-           self.rect.bottom > self.area.bottom:
-            self.slope = -self.slope
-            
-        if self.rect.left < self.area.left or \
-           self.rect.right > self.area.right:
-            self.move = -self.move
-            self.slope = -self.slope
-        newpos = self.rect.move((self.move,self.move*self.slope))
-        self.rect = newpos
-        
-    def reverse(self):
-            self.slope = -self.slope
-            
-            
-    def checkHit(self,target):
-        hitbox = target.rect.inflate(-2, -2)
-        
-        return hitbox.colliderect(self.rect)
-    
 def main():
     """this function is called when the program starts.
        it initializes everything it needs, then runs in
        a loop until the function returns."""
-#Initialize Everything
+# Initialize Everything
+    width = 800
+    height = 600
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption('break0ut')
     pygame.mouse.set_visible(0)
     
@@ -112,46 +27,94 @@ def main():
     background = background.convert()
     background.fill((0, 0, 0))
     
-    #Display The Background
+    # Display The Background
     #  screen.blit(background, (0, 0))
     #    pygame.display.flip()
 
-    #Prepare Game Objects
+    # Prepare Game Objects
     clock = pygame.time.Clock()
 
-    gameBar = Bar(screen.get_height()-50)
+    # create the bar and set the y position of the bar
+    gameBar = Bar(screen.get_height() - 50)
     
-
     playBall = Ball()
     
+    playBall.resetSelf(gameBar.rect)
+    
+    moveableSprites = pygame.sprite.RenderPlain((gameBar, playBall))
+    
+    # sample Block
+    aBlock = Block((10, 10))
+    
+    #create a pit bottom for the ball, which is used to reset the ball
+    pitBottom = Rect(0, height - 10, width, 10)
+    
+    #block area should occupy 80% of the width and 50% of height
+    blockAreaWidth = int(width * 0.8)
+    blockAreaHeight = int(height * 0.5)
+    
+    maxBlockInX = (blockAreaWidth/aBlock.rect.width)
+    
+    #update the block area width to the correct size
+    blockAreaWidth = (maxBlockInX*aBlock.rect.width)
+    
+    # get start position for x
+    blockStartX = int((width - (blockAreaWidth)) / 2)
+    blockStartY = int(blockStartX) # same difference above as on the sides
+    
+    
+    # create the blocks start at the x value and go until the max width has reached
+    # there should be no space between the current and the next block
+    gameAreaSprites = pygame.sprite.RenderPlain()
+    for blockPosX in xrange(blockStartX, (width - blockStartX), aBlock.rect.w):
+        for blockPosY in xrange(blockStartY, (blockAreaHeight + blockStartY), aBlock.rect.h):
+            gameAreaSprites.add(Block((blockPosX, blockPosY)))
+    
 
+    # draw background
     screen.blit(background, (0, 0))
     pygame.display.flip()
     
-    allsprites = pygame.sprite.RenderPlain((gameBar,playBall))
+   
+    # main game loop
     while 1:
         clock.tick(60)
 
-    #Handle Input Events
+    # Handle Input Events
         for event in pygame.event.get():
+            # quit the game if escape is pressed
             if event.type == QUIT:
                 return
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 return
+            # move the handles according to the object
+            elif event.type == MOUSEMOTION:
+                gameBar.handleEvent(event)
+                playBall.handleEvent(event)
+            elif event.type == MOUSEBUTTONDOWN:
+                playBall.handleEvent(event)
         
         
-        "if ball has hit the bar reverse the ball"        
-        if playBall.checkHit(gameBar):
-            playBall.reverse()
-
-
+        # check if a block has hit a ball, if so remove the block
+        # the ball is reversed in the checkhit method   
+        hit = playBall.checkHit(gameAreaSprites)    
+        if hit:
+            if hit.decreaseHP() == 0:
+                gameAreaSprites.remove(hit)
+            
+        gameBar.checkBarHit(playBall)
+        
+        if pitBottom.colliderect(playBall.rect):
+            playBall.resetSelf(gameBar.rect)
         
    
 
-    #Draw Everything
-        allsprites.update()
+    # update and draw Everything
+        gameAreaSprites.update()
+        moveableSprites.update()
         screen.blit(background, (0, 0))
-        allsprites.draw(screen)
+        moveableSprites.draw(screen)
+        gameAreaSprites.draw(screen)
         pygame.display.flip()
 
 
